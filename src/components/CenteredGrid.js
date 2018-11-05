c3d5810a837cca8698613ab0a766131ea0124825
Kevin Kafuku <kevkafuku@gmail.com>
2018-11-04 21:07:26 -0500

    Implemented Basic Material Ui Grid for layout purposes of the application

---
+++ src/components/CenteredGrid.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+ const styles = ({ spacing, palette }) => ({
+  root: { flexGrow: 1 },
+  paper: {
+    padding: spacing.unit * 2,
+    textAlign: "center",
+    color: palette.text.secondary
+  }
+});
+ const CenteredGrid = ({ classes }) => {
+  const { root, paper } = classes;
+   return (
+    <div className={root}>
+      <Grid container spacing={8}>
+        <Grid item xs={4}>
+          <Paper className={paper}>xs=6</Paper>
+        </Grid>
+        <Grid item xs={8}>
+          <Paper className={paper}>xs=6</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+ CenteredGrid.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+ export default withStyles(styles)(CenteredGrid); 
